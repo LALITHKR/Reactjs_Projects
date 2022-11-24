dd4e1c5fd7d9575550497169181dfa5ff8c847ee
LALITHKR <LALITH.186@GMAIL.COM>
2022-11-24 14:42:48 +0530

    commit

---
+++ moviesite_assgn/src/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <p>© 2022 Lalith Kumar Erraboina</p>
+        
+        
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
